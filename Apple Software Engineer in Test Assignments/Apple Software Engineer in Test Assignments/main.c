0ae1aeb6b7d99dfbbaeb0ac63934a321186f6d20
David Bulnes <dbulnes@lynchmarks.com>
2013-12-12 11:10:37 -0800

    Started XCode Project

---
+++ Apple Software Engineer in Test Assignments/Apple Software Engineer in Test Assignments/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Apple Software Engineer in Test Assignments
+//
+//  Created by David Bulnes on 12/12/13.
+//  Copyright (c) 2013 David Bulnes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
