5a3286eb62211e7e4a302e26e762604325266ab4
David Bulnes <dbulnes@lynchmarks.com>
2013-12-12 11:29:50 -0800

    Added Assignment 1 XCode Project

---
+++ Assignment_1/Assignment_1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Assignment_1
+//
+//  Created by David Bulnes on 12/12/13.
+//  Copyright (c) 2013 David Bulnes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
